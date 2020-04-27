c7874c1a46dfa62644c55acb7461b0225eef7b19
mahesh031 <mahesh031@gmail.com>
2020-04-27 10:02:57 +0530

    Create assume-role-with-env-variables.sh

---
+++ assume-role-with-env-variables.sh
@@ -0,0 +1,20 @@
+set -e
+PATH=$PATH:/usr/bin/jq
+export AWS_ACCESS_KEY_ID=${awsAccessKeyId}
+export AWS_SECRET_ACCESS_KEY=${awsSecretAccessKey}
+export AWS_DEFAULT_REGION=${awsRegion}
+export AWS_ROLE_ARN=${awsRoleArn}
+
+aws sts assume-role --role-arn "${AWS_ROLE_ARN}"  --role-session-name role-session >>/dev/null 2>&1
+
+unset  AWS_SESSION_TOKEN
+temp_role=$(aws sts assume-role \
+                    --role-arn "${AWS_ROLE_ARN}" \
+                    --role-session-name "role-session")
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs) >>/dev/null 2>&1
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs) >>/dev/null 2>&1
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs) >>/dev/null 2>&1
+
+
+aws s3 ls
