34eba5a59aa8b90e32778773a45eb5b1431282bf
Mahesh Kavatage <mkavatage@vmware.com>
2020-04-27 12:44:12 +0530

    Update with-profile-option.sh

--- with-profile-option.sh
+++ with-profile-option.sh
@@ -3,23 +3,23 @@ set -e
 
 #Create aws credentials file under temp directory
 AWS_CREDENTIALS_FILE=/tmp/.aws/credentials
-mkdir -p “${AWS_CREDENTIALS_FILE%/*}”
+mkdir -p "${AWS_CREDENTIALS_FILE%/*}"
 touch $AWS_CREDENTIALS_FILE
 chmod 600 $AWS_CREDENTIALS_FILE
-echo “[user]”                              > $AWS_CREDENTIALS_FILE
-echo “aws_access_key_id = xxxxxxxxx”         >> $AWS_CREDENTIALS_FILE
-echo “aws_secret_access_key = xxxxxxxx” >> $AWS_CREDENTIALS_FILE
-echo “region = xxxxxx” >> $AWS_CREDENTIALS_FILE
+echo "[user]"                              > $AWS_CREDENTIALS_FILE
+echo "aws_access_key_id = ${awsAccessKeyId}"         >> $AWS_CREDENTIALS_FILE
+echo "aws_secret_access_key = ${awsSecretAccessKey}" >> $AWS_CREDENTIALS_FILE
+echo "region = ${awsRegion}" >> $AWS_CREDENTIALS_FILE
 
 #Create aws config file under temp directory
 AWS_CONFIG_FILE=/tmp/.aws/config
-mkdir -p “${AWS_CONFIG_FILE%/*}”
+mkdir -p "${AWS_CONFIG_FILE%/*}"
 touch $AWS_CONFIG_FILE
 chmod 600 $AWS_CONFIG_FILE
-echo “[profile env-profile]”      > $AWS_CONFIG_FILE
-echo “role_arn = xxxxxxx”         >> $AWS_CONFIG_FILE
-echo “source_profile = user” >> $AWS_CONFIG_FILE
-echo “region = xxxxxxx" >> $AWS_CONFIG_FILE
+echo "[profile env-profile]"      > $AWS_CONFIG_FILE
+echo "role_arn = ${awsRoleArn}"         >> $AWS_CONFIG_FILE
+echo "source_profile = user" >> $AWS_CONFIG_FILE
+echo "region = ${awsRegion}" >> $AWS_CONFIG_FILE
 
 #use environment variable to override default file location
 export AWS_CONFIG_FILE=$AWS_CONFIG_FILE
